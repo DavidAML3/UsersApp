52cab43bfb9557772c2d0467ee48df969f342607
DavidAML3 <David.AML.3@hotmail.com>
2020-10-25 01:17:15 -0500

    Adde User struct

---
+++ UsersApp/Extensions-Utilities/UIView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Ext.swift
+//  UsersApp
+//
+//  Created by David Andres Mejia Lopez on 25/10/20.
+//
+
+import Foundation
---
+++ UsersApp/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  UsersApp
+//
+//  Created by David Andres Mejia Lopez on 25/10/20.
+//
+
+import UIKit
+
+struct User {
+    var id: Int
+    var name: String
+    var phone: String
+    var email: String
+}
