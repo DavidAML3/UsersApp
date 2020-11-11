f36fd0fee3f3513d22d5f6e5918009f79d0109f7
DavidAML3 <David.AML.3@hotmail.com>
2020-11-11 18:16:36 -0500

    Create images struct with two icons

--- UsersApp/Extensions-Utilities/Constants.swift
+++ UsersApp/Extensions-Utilities/Constants.swift
@@ -5,4 +5,9 @@
 //  Created by David Andres Mejia Lopez on 11/11/20.
 //
 
-import Foundation
+import UIKit
+
+struct images {
+    static let phone    = UIImage(named: "phone")!
+    static let mail     = UIImage(named: "mail")!
+}
